f9d5aa1119b84fa22a30d496fdf8f14656e017ce
kyle67 <daniels.kyle@gmail.com>
2019-12-19 16:21:50 -0500

    Modified index.html with div for carousel, created carousel.js in js directory.

--- js/script.js
+++ js/script.js
@@ -13,6 +13,25 @@ $(document).ready(function(){
         startDelay: 1000,
         showCursor: false
     });
+   
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+
+
 });
 
 
