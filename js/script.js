38235620d62bc4fed472795096d6134d8ac8eb95
kyle67 <daniels.kyle@gmail.com>
2019-12-23 04:32:27 -0500

    Styled navigation bar with scroll feature

--- js/script.js
+++ js/script.js
@@ -84,6 +84,14 @@ $(document).ready(function(){
 
        });
 
+       $("#navigation li a").click(function(e){
+             e.preventDefault();
+
+             var targetElement = $(this).attr("href")
+             var targetPosition = $(targetElement).offset().top;
+             $("html,body").animate({scrollTop: targetPosition - 50}, "slow");
+       });
+
          const nav = $("#navigation");
          const navTop =nav.offset().top;
 
@@ -94,9 +102,11 @@ $(document).ready(function(){
              const body = $("body");
 
              if($(window).scrollTop()>= navTop) {
+                 body.css("padding-top", nav.outerHeight() + "px");
                  body.addClass("fixedNav");
              }
              else{
+                 body.css("padding-top", 0);
                  body.removeClass("fixedNav");
              }
 
